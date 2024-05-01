686b598c62c56281b774f81077a04e99d91c5ed1
maparrat <ma.parat@uniandes.edu.co>
2024-04-30 20:30:55 -0500

    tag creado

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -14,6 +14,59 @@ Cypress.Commands.add('login', () => {
   cy.wait(2000);
 });
 
+// GIVEN: User is logged in
+describe('Crear nueva etiqueta ', () => {
+  beforeEach(() => {
+    // WHEN: User logs in before each test
+    cy.login();
+  });
+
+  // THEN: User creates a new tag with a known name "test"
+  it('Crea una nueva etiqueta con el nombre "test"', () => {
+    cy.visit('http://localhost:2368/ghost/#/tags/new');
+    cy.wait(2000);
+
+    // WHEN: User types the known tag name "test"
+    const tagName = 'test';
+    cy.get('#tag-name')
+      .should('be.visible')
+      .type(tagName);
+
+    // WHEN: User clicks on the "Save" button
+    cy.contains('Save').click();
+
+    cy.wait(5000);
+
+    // THEN: The new tag with the known name "test" should be created
+  });
+});
+// GIVEN: User is logged in
+describe('Validar tag creado ', () => {
+  beforeEach(() => {
+    // WHEN: User logs in before each test
+    cy.login();
+  });
+
+  // THEN: User go to published pages
+  it('display tag created', () => {
+    cy.visit('http://localhost:2368/ghost/#/posts?type=published');
+    cy.wait(2000);
+
+    // WHEN: User types the known tag name "test"
+    const url = faker.internet.url();
+    cy.get('[data-test-tag-select]')
+    .click();
+
+
+    cy.wait(5000);
+
+    // THEN: The new tag with the known name "test" should be created
+
+    cy.contains('test');
+    
+  });
+});
+
 
 // GIVEN: User is logged in
 describe('Crear nueva pagina con dominio diferente ', () => {
@@ -147,30 +200,5 @@ describe('Crear nueva pagina ', () => {
   });
 });
 
-// GIVEN: User is logged in
-describe('Crear nueva etiqueta ', () => {
-  beforeEach(() => {
-    // WHEN: User logs in before each test
-    cy.login();
-  });
 
-  // THEN: User creates a new tag with a known name "test"
-  it('Crea una nueva etiqueta con el nombre "test"', () => {
-    cy.visit('http://localhost:2368/ghost/#/tags/new');
-    cy.wait(2000);
-
-    // WHEN: User types the known tag name "test"
-    const tagName = 'test';
-    cy.get('#tag-name')
-      .should('be.visible')
-      .type(tagName);
-
-    // WHEN: User clicks on the "Save" button
-    cy.contains('Save').click();
-
-    cy.wait(5000);
-
-    // THEN: The new tag with the known name "test" should be created
-  });
-});
 
